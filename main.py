85357463de2b05e32255c34d6dad9d775eddf743
Esc9919 <emersoncaldeira1999@hotmail.com>
2025-10-18 17:28:38 -0300

    Ajuste para o lint

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+"""
+Este módulo implementa uma função de soma simples.
+"""
+
 def soma(a, b):
+    """Esta função retorna a soma de dois números."""
     return a + b
 
 if __name__ == "__main__":
     print(soma(2, 3))
+    
